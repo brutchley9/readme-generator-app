1c22b6035e3e2842b3ffc95b6ee981e286ef1ea0
brutchley <bhutchison0901@yahoo.com>
2023-11-18 15:06:12 -0600

    added starter code to index.js and generatemarkdown.js files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("Does it work?");
+
+
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+//const questions = [];
+
+// TODO: Create a function to write README file
+//function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+//function init() {}
+
+// Function call to initialize app
+//init();
